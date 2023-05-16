a6e5452b26a4ea057f9cb3a8f51bebd3930fb25f
Alexander Linder <alexander.linder@student.kit.edu>
2023-05-16 13:59:15 +0200

    Add function to ProcessedPacket to extract routing flag

--- src/main/java/com/robertsoultanaev/javasphinx/packet/ProcessedPacket.java
+++ src/main/java/com/robertsoultanaev/javasphinx/packet/ProcessedPacket.java
@@ -1,9 +1,20 @@
 package com.robertsoultanaev.javasphinx.packet;
 
 import com.robertsoultanaev.javasphinx.packet.header.PacketContent;
+import org.msgpack.core.MessagePack;
+
+import java.io.IOException;
 
 /**
  * Type to represent the return value of the mix node processing method
  */
 public record ProcessedPacket(byte[] tag, byte[] routing, PacketContent packetContent, byte[] macKey) {
+
+    public String routingFlag() throws IOException {
+        final var unpacker = MessagePack.newDefaultUnpacker(routing);
+        int routingLen = unpacker.unpackArrayHeader();
+        String flag = unpacker.unpackString();
+        unpacker.close();
+        return flag;
+    }
 }
